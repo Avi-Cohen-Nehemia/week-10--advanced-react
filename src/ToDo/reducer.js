7645167b439be85a1d1b20fbc559797c9cdbf099
Avi Cohen Nehemia <avrahamhcn@gmail.com>
2020-06-05 12:37:51 +0100

    solved 'addItem' set of exercises using reducer

---
+++ src/ToDo/reducer.js
@@ -0,0 +1,12 @@
+import React, { useReducer } from "react";
+
+export const addItem = (state, { value }) => {
+
+    return {
+        ...state,
+        items: [...state.items, {
+            task: value,
+            completed: false
+        }]
+    };
+};
---
+++ src/ToDo/reducer.test.js
@@ -0,0 +1,46 @@
+import reducer, {
+    addItem,
+    removeItem,
+    updateItem,
+    completeItem
+} from './reducer';
+
+const initialState = {
+    items: []
+};
+
+it('adds items', () => {
+    let result = addItem(initialState, { value: "Hello" });
+
+    // first item in the items array should be a new task with "Hello"
+    expect(result.items[0]).toEqual({ task: "Hello", completed: false });
+
+    // shouldn't be the same array we started with
+    expect(result.items).not.toBe(initialState.items);
+
+    // passing in the previous result, which already had one item
+    result = addItem(result, { value: "Mum" });
+
+    // should be two items
+    expect(result.items.length).toBe(2);
+
+    // check both items are now in items, in order given
+    expect(result.items[0]).toEqual({ task: "Hello", completed: false });
+    expect(result.items[1]).toEqual({ task: "Mum", completed: false });
+});
+
+it('removes items', () => {
+    // Removing tests here
+});
+
+it('updates items', () => {
+    // Updating tests here
+});
+
+it('completes items', () => {
+    // Completing tests here
+});
+
+it('reduces', () => {
+    // Reducer tests here
+});
