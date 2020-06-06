8ab69dfd6150f12bc2b603fd0ccffd6e4434416e
Avi Cohen Nehemia <avrahamhcn@gmail.com>
2020-06-06 11:43:23 +0100

    solved 'reducer' third test

--- src/ToDo/reducer.test.js
+++ src/ToDo/reducer.test.js
@@ -121,4 +121,16 @@ it('reduces', () => {
     newState = reducer(initialState, { type: "NEW_ITEM", value: "Hello" });
     // make sure it's been added
     expect(newState.items[0]).toEqual({ task: "Hello", completed: false });
+
+    // 3rd test: add some more items
+    newState = reducer(newState, { type: "NEW_ITEM", value: "Mum" });
+    newState = reducer(newState, { type: "NEW_ITEM", value: "How" });
+    newState = reducer(newState, { type: "NEW_ITEM", value: "Are" });
+    newState = reducer(newState, { type: "NEW_ITEM", value: "You" });
+    // check they've been added
+    expect(newState.items.length).toBe(5);
+    // remove item at index 2
+    newState = reducer(newState, { type: "REMOVE_ITEM", index: 2 });
+    // check index 3 has moved down to index 2
+    expect(newState.items[2]).toEqual({ task: "Are", completed: false });
 });
